4234c612d298bf47bac113b5c95d54ea39923e4c
Naveen-005 <naveenalex512@gmail.com>
2025-04-10 21:38:41 +0530

    code refactored

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import whisper
 
+def convert_time_stamp(time):
+    hr=int(time/3600)
+    min=int((time/60)-hr*60)
+    sec=int(time-hr*3600-min*60)
+    ms=int((time*100)%100)
+
+    return(('%02d' % hr)+':'+('%02d' % min)+':'+('%02d' % sec)+','+('%03d' % ms))
+
 def generate_subtitles(input_audio,output_path,Model="small"):
 
     model = whisper.load_model(Model)
     result = model.transcribe(input_audio,word_timestamps=True)
-    #print(result["text"])
-    #print(result["segments"])
+
     fp = open(output_path, "w")
+    seq=0
 
     for line in result["segments"]:
 
         srt=""
-        seq=str(line['id']+1)
-        hr=int(line['start']/3600)
-        min=int((line['start']/60)-hr*60)
-        sec=int(line['start']-hr*3600-min*60)
-        ms=int((line['start']*100)%100)
-        #print(srt)
-        srt+=(str(seq)+'\n'+('%02d' % hr)+':'+('%02d' % min)+':'+('%02d' % sec)+','+('%03d' % ms)+' --> ')
-        #print(srt)
-        hr=int(line['end']/3600)
-        min=int((line['end']/60)-hr*60)
-        sec=int(line['end']-hr*3600-min*60)
-        ms=int((line['end']*100)%100)
-        srt+=(('%02d' % hr)+':'+('%02d' % min)+':'+('%02d' % sec)+','+('%03d' % ms)+'\n')
-        #print(srt)
+        seq+=1
+
+        time=convert_time_stamp(line['start'])
+        srt+=(str(seq)+'\n'+time+' --> ')
+
+        time=convert_time_stamp(line['end'])
+        srt+=(time+'\n')
         srt+=(line['text']+'\n\n')
-        #print(srt,"\n\n")
+        
         fp.write(srt)
-        #print(line)
         
     fp.close()
 
